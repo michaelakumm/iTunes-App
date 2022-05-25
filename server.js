05bb35c4803126688254eaa204a7f6946cb941c1
[Michaela Kumm] <[mkumm0799239321@gmail.com]>
2022-05-25 20:14:10 +0200

    cors options removed

--- server.js
+++ server.js
@@ -16,23 +16,7 @@ const app = express();
 
 // App middleware
 app.use(express.json());
-
-// const whitelist = ['http://localhost:3000', 'http://localhost:3030', 'https://michaelakumm-itunes-app.herokuapp.com'];
-// const corsOptions = {
-//    origin: function (origin, callback) {
-//       console.log("** Origin of request " + origin);
-//       if (whitelist.indexOf(origin) !== -1 || !origin) {
-//          console.log("Origin acceptable");
-//          callback(null, true);
-//       } else {
-//          console.log("Origin rejected");
-//          callback(new Error('Not allowed by CORS'))
-//       }
-//    }
-// }
-// app.use(cors(corsOptions));
 app.use(cors());
-
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({
    extended: true
