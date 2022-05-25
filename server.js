7b76340f27fe4d11a70caccd0329febaaa69ab45
[Michaela Kumm] <[mkumm0799239321@gmail.com]>
2022-05-25 14:26:11 +0200

    update file

--- server.js
+++ server.js
@@ -95,7 +95,7 @@ if (process.env.NODE_ENV === 'production') {
    app.use(express.static(path.join(__dirname, 'frontend/build')));
    // Handle React routing, return all requests to React app
    app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
+      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
    });
 }
 
